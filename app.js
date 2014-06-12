f99208cc79f67b7730c3c8e02728f813ed4ba62f
Paul Adenot <paul@paul.cx>
2014-06-12 15:23:31 +0200

    playback somewhat working.

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ connection.onerror = function (error) {
 };
 
 connection.onmessage = function (message) {
-    connection.send(message.data);
+  var obj = JSON.parse(message.data);
+  if (obj.i == 1) {
+    s.trigger(obj.x);
+  }
 };
 
 window.onbeforeunload = function() {
@@ -18,3 +21,88 @@ window.onbeforeunload = function() {
   connection.onclose = function () {}; // disable onclose handler first
   connection.close()
 };
+
+
+function getFile(url, cb) {
+  var request = new XMLHttpRequest();
+  request.open("GET", url, true);
+  request.responseType = "arraybuffer";
+
+  request.onload = function() {
+    ctx.decodeAudioData(request.response, function(data) {
+      cb(data);
+    }, function() {
+      alert("decodeAudioData error");
+    });
+  };
+
+  request.send();
+}
+
+function Sample(sink, url, line, device, loaded_cb) {
+  this.url = url;
+  this.buffer_source ;
+  this.audio_buffer = 
+  this.sink = sink;
+  this.current_button = 0;
+  this.line = line;
+  this.device = device;
+  this.loaded_cb = loaded_cb;
+}
+
+Sample.prototype.init = function() {
+  getFile(this.url, this.loaded.bind(this));
+}
+
+Sample.prototype.loaded = function(data) {
+  this.audio_buffer = data;
+  this.loaded_cb();
+}
+
+// index between 0 and 15
+Sample.prototype.trigger = function(index) {
+  if (index < 0 && index > 15) {
+    throw "bad index";
+  }
+  if (this.buffer_source) {
+    this.buffer_source.stop(0);
+    clearInterval(this.progress_itv)
+    this.progress_itv = 0;
+    // turn the led off;
+    // XXX fix
+    // device.led(this.current_button, this.line, 0);
+  }
+  this.current_button = index;
+  this.buffer_source = this.sink.context.createBufferSource();
+  this.buffer_source.buffer = this.audio_buffer;
+  this.buffer_source.loop = true;
+  var button_length = this.audio_buffer.duration / 16;
+  var offset_seconds = index * button_length;
+  console.log(button_length);
+  this.buffer_source.start(0, offset_seconds, this.audio_buffer.duration)
+  this.progress_itv = setInterval(this.progress.bind(this), button_length * 1000);
+  this.buffer_source.connect(this.sink);
+}
+
+Sample.prototype.progress = function() {
+  // turn the led off;
+  // device.led(this.current_button, this.line, 0);
+  var off = {x: this.current_button, y: this.line, i:0};
+  connection.send(JSON.stringify(off));
+  this.current_button = (this.current_button + 1) % 16;
+  // console.log(this.current_button);
+  // turn the led on
+  // device.led(this.current_button, this.line, 1);
+  var on = {x: this.current_button, y: this.line, i:1};
+  connection.send(JSON.stringify(on));
+}
+
+var ctx = new AudioContext();
+var device = {};
+s = new Sample(ctx.destination, "think-looped-mono.opus", 0, device, function() {
+  console.log("loaded");
+  // s.trigger(3);
+});
+
+s.init();
+
